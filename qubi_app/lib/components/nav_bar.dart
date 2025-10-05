c4d7ded5b1ab505b464e35be4b66e1b22e45f7bd
Phoebe Dainer <pdainer@terpmail.umd.edu>
2025-10-05 14:48:37 -0400

    Fix navbar to have icons

--- qubi_app/lib/components/nav_bar.dart
+++ qubi_app/lib/components/nav_bar.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
 import '../pages/home/qubis_page.dart'; //home page
 import '../pages/learn/learn_page.dart'; //learning page
 import '../pages/profile/profile_page.dart'; //user profile page
@@ -21,6 +23,22 @@ class _NavBarState extends State<NavBar> {
     ProfilePage(),
   ];
 
+  // Image icons for each page
+  final List<Map<String, String>> _iconPaths = [
+    {
+      'normal': 'assets/images/home_icon.svg',
+      'active': 'assets/images/home_gradient_icon.svg',
+    },
+    {
+      'normal': 'assets/images/learn_icon.svg',
+      'active': 'assets/images/learn_gradient_icon.svg',
+    },
+    {
+      'normal': 'assets/images/profile_icon.svg',
+      'active': 'assets/images/profile_gradient_icon.svg',
+    },
+  ];
+
   // Navigate to page when a tab is clicked
   void _onItemTapped(int index) {
     setState(() {
@@ -28,68 +46,33 @@ class _NavBarState extends State<NavBar> {
     });
   }
 
-  // Light up the icon with gradient colors when selected
-  Widget _buildIcon(IconData icon, bool isSelected) {
-    if (isSelected) { 
-      return Container(
-        padding: const EdgeInsets.all(6),
-        decoration: const BoxDecoration(
-          shape: BoxShape.circle,
-          gradient: LinearGradient(
-            //colors retrieved from Figma brand palette
-            colors: [Color(0xFF6525FE), Color(0xFF1A91FC)],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-          ),
-        ),
-        child: const Icon(Icons.circle, color: Colors.white, size: 18),
-      );
-    } else {
-      return Icon(icon, color: Colors.black54);
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
+    // Get BottomNavigationBar height (default 56 if not specified)
+    double navBarHeight = kBottomNavigationBarHeight;
+    double iconWidth = navBarHeight * 75 / 48; // maintain aspect ratio
+
     return Scaffold(
       body: _pages[_selectedIndex],
-      bottomNavigationBar: Container(
-        decoration: BoxDecoration(
-          color: Colors.white,
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withValues(alpha: 0.05),
-              blurRadius: 8,
-              offset: const Offset(0, -2),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        items: List.generate(_iconPaths.length, (index) {
+          return BottomNavigationBarItem(
+            //icon is lit up if selected, else is not
+            icon: SvgPicture.asset(
+              _selectedIndex == index
+                  ? _iconPaths[index]['active']!
+                  : _iconPaths[index]['normal']!,
+              height: navBarHeight,
+              width: iconWidth,
             ),
-          ],
-        ),
-        child: BottomNavigationBar(
-          currentIndex: _selectedIndex,
-          onTap: _onItemTapped,
-          backgroundColor: Colors.white,
-          type: BottomNavigationBarType.fixed,
-          showSelectedLabels: true,
-          showUnselectedLabels: true,
-          selectedFontSize: 12,
-          unselectedFontSize: 12,
-          items: [ //icons in the navigation bar
-            BottomNavigationBarItem(
-              icon: _buildIcon(Icons.circle_outlined, _selectedIndex == 0),
-              label: 'Qubis',
-            ),
-            BottomNavigationBarItem(
-              icon: _buildIcon(Icons.menu_book_outlined, _selectedIndex == 1),
-              label: 'Learn',
-            ),
-            BottomNavigationBarItem(
-              icon: _buildIcon(Icons.person_outline, _selectedIndex == 2),
-              label: 'Profile',
-            ),
-          ],
-        ),
+            label: '',
+          );
+        }),
       ),
     );
   }
-
 }
