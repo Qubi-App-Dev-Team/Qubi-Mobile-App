95a7d785ef52da9cda7d4f0ed79ac17da3f2d332
Joey Lee <66647435+JoeyLee-22@users.noreply.github.com>
2025-10-06 19:42:14 -0400

    organization

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from firebase_reader import get_circuit
-from create_circuit import create_circuit
-from send_ionq import get_ionq_results
+from utils.firebase_reader import get_circuit
+from utils.create_circuit import create_circuit
+from utils.send_ionq import get_ionq_results
 
 gates, num_qubits, num_clbits = get_circuit()
+qc = create_circuit(gates, num_qubits, num_clbits)
 
-print(gates, num_qubits, num_clbits)
-print(create_circuit(gates, num_qubits, num_clbits))
+print(f"Created Circuit:\n{qc}\n")
 
-print(get_ionq_results(create_circuit(gates, num_qubits, num_clbits), shots=1000, backend_name="ionq_simulator", create_plot=True, save_plot="histogram.png"))
+get_ionq_results(qc, shots=1000, backend_name="ionq_simulator", create_plot=True, save_plot="results/histogram.png")
