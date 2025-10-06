5ec54d48228ae3e78b99f0315be3949d5e93ecdb
Alex Shrestha <ashrest6@terpmail.umd.edu>
2025-10-06 18:36:47 -0400

    fastapi version up

--- backend/app.py
+++ backend/app.py
@@ -2,8 +2,11 @@ import firebase_admin
 from firebase_admin import firestore, credentials
 import os, json
 from dotenv import load_dotenv
+from fastapi import FastAPI
 
 # it prints error message but that should be fine
+
+app = FastAPI()
 
 # Env creds
 load_dotenv()
@@ -12,41 +15,58 @@ firebase_creds_dict = json.loads(firebase_creds_str)
 cred = credentials.Certificate(firebase_creds_dict)
 
 # init and create app
-app = firebase_admin.initialize_app(cred)
+application = firebase_admin.initialize_app(cred)
 db = firestore.client()
 
-# Creation
-ref = db.collection("runs").document("todelete").set({
-    "circuit_id": 1738,
-    "depth": 10,
-    "results": 101,
-    "time": {
-        "total": 10,
-        "execution": 4,
-        "pending": 6,
-    },
-    "per_shot": 100,
-    "quantum_computer": "IBM",
-})
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
-# Reading
-runs = db.collection("runs").stream()
-count = 1
-for run in runs:
-    print(f"{run.id} => {count}")
-    count += 1
+@app.post("/runs")
+async def post_run():
+    # Creation
+    ref = db.collection("runs").document("todelete").set({
+        "circuit_id": 1738,
+        "depth": 10,
+        "results": 101,
+        "time": {
+            "total": 10,
+            "execution": 4,
+            "pending": 6,
+        },
+        "per_shot": 100,
+        "quantum_computer": "IBM",
+    })
+    return {"message": "It ran."}
 
-# Updating
-document = db.collection("runs").document("testing")
-document.update({
-    "depth": 20,
-    "quantum_computer": "IonQ",
-})
+@app.get("/runs/{id_number}")
+async def get_run(id_number: str):
+    # Reading
+    run = db.collection("runs").document(id_number).get()
+    if run.exists:
+        return run.to_dict()
+    else:
+        return {"error": "Run not found"}
 
-# Deleting
-db.collection("runs").document("testing").update({
-    "results": firestore.DELETE_FIELD
-})
+@app.put("/runs/{computer_name}/{depth}")
+async def update_run(computer_name: str):
+    # Updating
+    document = db.collection("runs").document("testing")
+    document.update({
+        "depth": depth,
+        "quantum_computer": computer_name,
+    })
+    return {"message": "It worked"}
 
-db.collection("runs").document("todelete").delete()
+
+@app.delete("/runs/{id_number}")
+async def delete_run(id_number: int):
+    # Deleting
+    db.collection("runs").document(id_number).update({
+        "results": firestore.DELETE_FIELD
+    })
+
+    db.collection("runs").document("todelete").delete()
+
+    return {"message": "Deleted"}
 
