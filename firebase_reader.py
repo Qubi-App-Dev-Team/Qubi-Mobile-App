bdf51ce88bfe5028004b369c63f85413f276ca68
Joey Lee <66647435+JoeyLee-22@users.noreply.github.com>
2025-10-06 19:07:58 -0400

    fix different loading methods and separate fucntions

--- firebase_reader.py
+++ firebase_reader.py
@@ -1,23 +1,21 @@
-import os
-import json
 from dotenv import load_dotenv
+
+import json
+import os
 
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-from qiskit import QuantumCircuit
-
-
 load_dotenv()
 
 # Option 1: Use environment variable (requires FIREBASE_CREDENTIALS to be set)
-# cred_dict = json.loads(os.getenv('FIREBASE_CREDENTIALS'))
-# cred = credentials.Certificate(cred_dict)
+cred_dict = json.loads(os.getenv('FIREBASE_CREDENTIALS'))
+cred = credentials.Certificate(cred_dict)
 
 # Option 2: Use service account key file (recommended)
-cred = credentials.Certificate('service-account-key.json')
-firebase_admin.initialize_app(cred)
+# cred = credentials.Certificate('service-account-key.json')
 
+firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 def get_circuit():
@@ -29,20 +27,3 @@ def get_circuit():
         num_clbits = circuit.to_dict()['num_clbits']
 
         return (gates, num_qubits, num_clbits)
-
-def make_circuit(gates, num_qubits, num_clbits):
-    qc = QuantumCircuit(num_qubits, num_clbits)
-    for gate in gates:
-
-        if gate['name'] == "h":
-            qc.h(*gate['qubits'])
-        elif gate['name'] == "x":
-            qc.x(*gate['qubits'])
-        elif gate['name'] == "cx":
-            qc.cx(*gate['qubits'])
-        elif gate['name'] == "rz":
-            qc.rz(gate['params'][0], *gate['qubits'])
-        elif gate['name'] == "measure":
-            qc.measure(gate['qubits'], gate['clbits'])
-        
-    return qc
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from firebase_reader import get_circuit, make_circuit
+from firebase_reader import get_circuit
+from create_circuit import create_circuit
 
 gates, num_qubits, num_clbits = get_circuit()
 
 print(gates, num_qubits, num_clbits)
-print(make_circuit(gates, num_qubits, num_clbits))
+print(create_circuit(gates, num_qubits, num_clbits))
