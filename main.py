02747591142f5dccb5ba6e1afb8da7078aba760d
kanjonavosabud <ksabud@terpmail.umd.edu>
2025-10-06 19:02:08 -0400

    make circuit added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from firebase_reader import get_circuit
+from firebase_reader import get_circuit, make_circuit
 
 gates, num_qubits, num_clbits = get_circuit()
 
 print(gates, num_qubits, num_clbits)
+print(make_circuit(gates, num_qubits, num_clbits))
