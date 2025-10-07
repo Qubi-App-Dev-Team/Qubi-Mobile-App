d0fe5c59ac58cf1a8f686bd52ad968e649d5d7e1
nahomwondimu <nahomawt@gmail.com>
2025-10-06 20:39:04 -0400

    fix backdrop

--- qubi_app/lib/components/app_backdrop.dart
+++ qubi_app/lib/components/app_backdrop.dart
@@ -1,33 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
+// components/app_backdrop.dart
 class AppBackdrop extends StatelessWidget {
-  final Widget child;
+  final Widget? child;
   final String assetPath;
 
   const AppBackdrop({
     super.key,
-    required this.child,
-    this.assetPath = 'assets/images/qubi_bg.svg', // Provide filepath for the svg here
+    this.child,
+    this.assetPath = 'assets/images/light_bg.svg',
   });
 
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        // SVG fills the entire screen behind everything
         Positioned.fill(
           child: SvgPicture.asset(
             assetPath,
-            fit: BoxFit.cover, // cover to avoid letterboxing
+            fit: BoxFit.cover,
             alignment: Alignment.center,
-            // semanticsLabel: 'App background', optional a11y label
           ),
         ),
-        // Page content on top
-        Positioned.fill(
-          child: child,
-        ),
+        if (child != null) Positioned.fill(child: child!),
       ],
     );
   }
