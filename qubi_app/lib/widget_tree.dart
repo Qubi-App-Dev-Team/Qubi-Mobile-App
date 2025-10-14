d6a4683098a2edbc7df167edca1fbe5f2f783a92
Alex Shrestha <ashrest6@terpmail.umd.edu>
2025-10-14 18:44:11 -0400

    firebase ui w register, login, forgot password

--- qubi_app/lib/widget_tree.dart
+++ qubi_app/lib/widget_tree.dart
@@ -1,8 +1,9 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:qubi_app/auth.dart';
 import 'package:qubi_app/pages/home/login.dart';
 import 'package:qubi_app/pages/home/home.dart';
+import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 
 class WidgetTree extends StatefulWidget {
   const WidgetTree ({Key? key}) : super(key: key);
@@ -13,21 +14,54 @@ class WidgetTree extends StatefulWidget {
 
 class _WidgetTreeState extends State<WidgetTree> {
   @override
-  Widget build(BuildContext context) {
+    Widget build(BuildContext context) {
     return StreamBuilder<User?>(
-      stream: Auth().authStateChanges,
+      stream: FirebaseAuth.instance.authStateChanges(),
       builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.active) {
-          final User? user = snapshot.data;
-          return user == null ? const LoginPage() : const HomePage();
-        } else {
-          return const Scaffold(
-            body: Center(
-              child: CircularProgressIndicator(),
-            ),
-          );
+        if (!snapshot.hasData) {
+          return SignInScreen(                                         
+            providers: [EmailAuthProvider()],
+            headerBuilder: (context, constraints, shrinkOffset) {
+              return Padding(
+                padding: const EdgeInsets.all(20),
+                child: AspectRatio(
+                  aspectRatio: 1,
+                  child: Image.asset('assets/images/qubi_ball.png'),
+                ),
+              );
+            },
+            subtitleBuilder: (context, action) {                 
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: action == AuthAction.signIn
+                    ? const Text('Welcome to Qubi App, please sign in!')
+                    : const Text('Welcome to Qubi App, please sign up!'),
+              );
+            },                                                      
+
+          );                                                           
         }
+
+        return const HomePage();
       },
     );
   }
+
+  // Widget build(BuildContext context) {
+  //   return StreamBuilder<User?>(
+  //     stream: Auth().authStateChanges,
+  //     builder: (context, snapshot) {
+  //       if (snapshot.connectionState == ConnectionState.active) {
+  //         final User? user = snapshot.data;
+  //         return user == null ? const LoginPage() : const HomePage();
+  //       } else {
+  //         return const Scaffold(
+  //           body: Center(
+  //             child: CircularProgressIndicator(),
+  //           ),
+  //         );
+  //       }
+  //     },
+  //   );
+  // }
 }
