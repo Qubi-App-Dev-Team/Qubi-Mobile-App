42e0887e33e531e7ebbc9117a97a70b3651868b7
Alex Shrestha <ashrest6@terpmail.umd.edu>
2025-10-23 14:19:00 -0400

    chrome functionality for users + email/password page after google

--- qubi_app/lib/main.dart
+++ qubi_app/lib/main.dart
@@ -4,12 +4,17 @@ import 'auth_gate.dart'; // widget tree for auth
 import 'package:firebase_core/firebase_core.dart'; // firebase core
 import 'firebase_options.dart'; // firebase options
 import 'package:flutter_dotenv/flutter_dotenv.dart'; // dotenv for env variables
+import 'package:flutter/foundation.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
-  final clientId = dotenv.env['IOS_CLIENT_ID'] ?? '';
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  final clientId = switch (defaultTargetPlatform) {
+    TargetPlatform.iOS || TargetPlatform.macOS => dotenv.env['IOS_CLIENT_ID'] ?? '',
+    TargetPlatform.android => '', // Android uses google-services.json instead
+    _ => dotenv.env['GOOGLE_CLIENT_ID'] ?? '', // Web or others
+  };
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
   runApp(MyApp(clientId: clientId));
 }
 
