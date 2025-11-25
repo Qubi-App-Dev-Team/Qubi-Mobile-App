07d31bb4aad0f0534c3a4590932b086b6354fab7
kanjonavosabud <ksabud@terpmail.umd.edu>
2025-11-24 19:43:07 -0500

    kj dits final

--- backend/quantum_new.py
+++ backend/quantum_new.py
@@ -37,55 +37,62 @@ def send_circuit(
     quantum_computer_type: str,
     shots: int
 ):
-   
-    user_info = get_user_info(user_id)
-    print(f"User info: {user_info}")
-
-    gates = circuit.get("gates")
-    num_qubits = circuit.get("num_qubits")
-    num_clbits = circuit.get("num_clbits")
-
-    print(f"Creating circuit...")
-    qc = create_circuit(gates, num_qubits, num_clbits)
-
-    db.collection("run_requests").document(run_request_id).update({
-        "status": "RUNNING"
-    })
-
-
-    print(f"Running circuit on {quantum_computer_type}...")
+    success = True
     start_time = time.perf_counter()
-    res = get_circuit_results(qc, shots=shots, quantum_computer_type=quantum_computer_type, user_info=user_info)
+    try:
+        user_info = get_user_info(user_id)
+        print(f"User info: {user_info}")
+
+        gates = circuit.get("gates")
+        num_qubits = circuit.get("num_qubits")
+        num_clbits = circuit.get("num_clbits")
+
+        print(f"Creating circuit...")
+        qc = create_circuit(gates, num_qubits, num_clbits)
+
+        db.collection("run_requests").document(run_request_id).update({
+            "status": "RUNNING"
+        })
+
+        print(f"Running circuit on {quantum_computer_type}...")
+        res = get_circuit_results(qc, shots=shots, quantum_computer_type=quantum_computer_type, user_info=user_info)
+
+        if res is None:
+            print("Failed to get results from quantum computer")
+            raise ValueError("Invalid quantum computer type")
+    
+    except Exception as e:
+        success = False
+        res = {}
+    
     elapsed_time = time.perf_counter() - start_time
 
-    if res is None:
-        print("Failed to get results from quantum computer")
-        return None
-
     res.update({
+        "success": success,
         "elapsed_time": elapsed_time,
         "run_request_id": run_request_id,
         "user_id": user_id,
-        "circuit_id": circuit_id,
+        "circuit_id": circuit_id
     })
-    
-    print(f"Results: {res}")
 
+    print(f"Results: {res}")
     print(f"Saving results to Firebase...")
     run_id = add_results_new(res)
     print("run_id: ", run_id)
 
     return run_id
 
-send_circuit(
-    run_request_id="123",
-    user_id="user_01",
-    circuit_id="circuit_01",
-    circuit={
-        "gates": [{"name": "h", "qubits": [0]}, {"name": "cx", "qubits": [0, 1]}, {"name": "measure", "qubits": [0, 1], "clbits": [0, 1]}],
-        "num_qubits": 2,
-        "num_clbits": 2
-    },
-    quantum_computer_type="ionq",
-    shots=1000
-)
+# Example usage
+
+# send_circuit(
+#     run_request_id="0J77V36f36AJqAT8MFIf",
+#     user_id="YIcqRYGvenTVAMxpgwXPXA5rOyi2",
+#     circuit_id="circuit_01",
+#     circuit={
+#         "gates": [{"name": "h", "qubits": [0]}, {"name": "cx", "qubits": [0, 1]}, {"name": "measure", "qubits": [0, 1], "clbits": [0, 1]}],
+#         "num_qubits": 2,
+#         "num_clbits": 2
+#     },
+#     quantum_computer_type="ionq",
+#     shots=1000
+# )
